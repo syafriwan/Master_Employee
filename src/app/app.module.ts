f9380fc4bb59be575ba3c693fe76f406f66d4833
febrisyafriwan <febrisyafriwan@gmail.com>
2020-09-01 17:45:08 +0700

    3 files modified, 5 added, 2 deleted

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from "@angular/platform-browser";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { AppComponent } from "./app.component";
+import { DemoMaterialModule } from "./material-module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {HttpClientModule,HTTP_INTERCEPTORS} from '@angular/common/http';
+// import {DialogComponent} from './helpers/dialog/dialog.component'
+@NgModule({
+  imports: [
+    BrowserModule,
+    DemoMaterialModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  entryComponents: [],
+  providers: [
+  ]
+})
+export class AppModule {}
