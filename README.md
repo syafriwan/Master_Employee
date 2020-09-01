ea56f0eec0fa19d1647fa73e2d89ea594398e722
febrisyafriwan <febrisyafriwan@gmail.com>
2020-09-01 17:01:57 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-fy3zt5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-fy3zt5)
---
+++ src/app/datepicker-overview-example.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+/** @title Basic datepicker */
+@Component({
+  selector: 'datepicker-overview-example',
+  templateUrl: 'datepicker-overview-example.html',
+})
+export class DatepickerOverviewExample {}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
