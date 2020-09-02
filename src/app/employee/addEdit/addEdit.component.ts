7846c95b3a4d58ca22ae1c560fe552c6ba9a59c0
febrisyafriwan <febrisyafriwan@gmail.com>
2020-09-02 21:29:07 +0700

    3 files modified

--- src/app/employee/addEdit/addEdit.component.ts
+++ src/app/employee/addEdit/addEdit.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+interface Food {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: "addEdit",
   templateUrl: "./addEdit.component.html",
   styleUrls: ["./addEdit.component.css"]
 })
+
 export class AddEditComponent {
+ selectedValue: string;
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
 
 }
