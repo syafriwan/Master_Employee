6bc40f9a95b90e731881989ebef8418446f9781d
febrisyafriwan <febrisyafriwan@gmail.com>
2020-09-02 10:11:15 +0700

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,47 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { EmployeeService } from "./providers/employee.service";
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  
+export class AppComponent implements OnInit   {
+    employees = [
+      {
+birthDate: "2020-06-07T17:00:00.000+00:00",
+gender: 1,
+id: 1,
+idNumber: 1,
+isDelete: 1,
+name: "rahma",
+position: {
+  code: "PRG",
+id: 3,
+isDelete: 0,
+name: "Programmer",
+}
+      }
+    ];
 
-    constructor() { }
+    constructor(
+        private employeeService: EmployeeService
+    ) { }
 
     ngOnInit() {
   
     }
-}
+
+    getEmployees() {
+    
+      this.employeeService.getEmployees().subscribe(rs => {
+          console.log(rs);
+          this.employees = rs.content;
+         console.log(this.employees[0].name )
+        },
+        error => {
+          console.log(error);
+     
+        }
+      );
+    }
+  }
---
+++ src/app/providers/employee.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from "@angular/core";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse
+} from "@angular/common/http";
+import { BehaviorSubject, Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
+@Injectable({ providedIn: "root" })
+export class EmployeeService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getEmployees(body: any): Observable<any> {
+    let url = "http://localhost:8080/employees";
+    let response: any;
+    let headers = new HttpHeaders({
+      "Content-Type": "application/json"
+      // 'X-Requested-Url': url,
+      // 'X-Requested-Method': 'POST',
+      // 'Authorization': Authorization
+    });
+    let options = { headers: headers };
+
+    return this.http
+      .get(url, body, options)
+      .pipe(map(this.extractData))
+  }
+
+  private extractData(body: any) {
+    return Object.assign(body);
+  }
+  private handleError(error: HttpErrorResponse | any) {
+    // In a real world app, we might use a remote logging infrastructure
+    let errMsg: string;
+    let errObj: any;
+
+    if (error instanceof HttpErrorResponse) {
+      const err = error.message || JSON.stringify(error);
+      errMsg = `${error.status} - ${error.statusText || ""} ${err}`;
+      errObj = error.message;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+      const body = error.message || "";
+      errObj = body;
+    }
+    return Observable.throw(error.status);
+  }
+}
