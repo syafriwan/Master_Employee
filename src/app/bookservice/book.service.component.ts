f6131a0fb0691c6607e806ba9844a91294e00702
febrisyafriwan <febrisyafriwan@gmail.com>
2021-08-07 03:20:10 +0700

    3 files modified

--- src/app/bookservice/book.service.component.ts
+++ src/app/bookservice/book.service.component.ts
@@ -62,6 +62,6 @@ export class BookServiceComponent implements OnInit {
   }
   getHeight() {
     console.log(this.innerHeight);
-    return this.innerHeight - 100;
+    return this.innerHeight - 150;
   }
 }
