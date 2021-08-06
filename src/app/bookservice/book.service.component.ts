1c5ff3d24b55ed6cc4b91456210ba50707ec746f
febrisyafriwan <febrisyafriwan@gmail.com>
2021-08-06 21:58:31 +0700

    1 file modified

--- src/app/bookservice/book.service.component.ts
+++ src/app/bookservice/book.service.component.ts
@@ -10,12 +10,7 @@ export class BookServiceComponent implements OnInit {
   ngOnInit() {
     this.innerWidth = window.innerWidth;
   }
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    this.innerWidth = window.innerWidth;
-  }
   getHeight(){
-    console.log(this.innerWidth)
-    return this.innerWidth
+    return this.innerWidth - 50
   }
 }
