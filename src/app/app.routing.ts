eb99bffd6b2471a7b2800a7d26c022f9b03b7823
febrisyafriwan <febrisyafriwan@gmail.com>
2021-08-06 19:22:10 +0700

    1 file modified

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,30 +1,30 @@
 // app.routing.ts
 // App Routing as a Module
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { EmployeeComponent } from "./employee/employee.component";
-import { BookServiceComponent } from "./bookservice/book.service.component";
-import { AddEditComponent } from "./employee/addEdit/addEdit.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+import { BookServiceComponent } from './bookservice/book.service.component';
+import { AddEditComponent } from './employee/addEdit/addEdit.component';
 // Routes
 const routes: Routes = [
   {
-    path: "",
-    redirectTo: "promise/karyawanindex",
+    path: '',
+    redirectTo: 'book/service',
     pathMatch: 'full'
   },
   {
-    path: "promise/karyawanindex",
+    path: 'promise/karyawanindex',
     component: EmployeeComponent
   },
   {
-    path: "promise/karyawaneditadd",
+    path: 'promise/karyawaneditadd',
     component: AddEditComponent
   },
   {
-    path: "book/service",
+    path: 'book/service',
     component: BookServiceComponent
   },
-  { path: "**", redirectTo: "book/service" }
+  { path: '**', redirectTo: 'book/service' }
 ];
 
 @NgModule({
